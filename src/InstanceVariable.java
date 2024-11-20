5eb883c41df4c193faed97e9749728925746e74c
kdevsharma02 <kdevsharma012@gmail.com>
2024-11-20 19:05:10 +0530

    Checking the instance variable

---
+++ src/InstanceVariable.java
@@ -0,0 +1,9 @@
+public class InstanceVariable {
+    int a;
+    public static void main(String[] args){
+        InstanceVariable var=new InstanceVariable();
+        var.a=10;
+        System.out.println(var.a);
+    }
+
+}
